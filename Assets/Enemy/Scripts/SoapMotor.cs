2edd05db5648ba040a2b58c5807d9b098f3035fa
malcovys <malcovys22.aps@gmail.com>
2025-01-23 20:14:44 +0300

    Enhance SoapMotor behavior: implement jump functionality with target position movement and trigger bubble projections upon arrival

--- Assets/Enemy/Scripts/SoapMotor.cs
+++ Assets/Enemy/Scripts/SoapMotor.cs
@@ -1,15 +1,30 @@
+using System.Threading;
 using UnityEngine;
 
 public class SoapMotor : MonoBehaviour
 {
     [SerializeField] BubbleProjections bubbleProjections;
     [SerializeField] BreathOfBubbles breathOfBubbles;
+    [SerializeField] float jumpSpeed = 5f;
+
+    private Vector3 jupmTargetPosition = Vector3.zero;
+
+    void FixedUpdate() {
+        if(jupmTargetPosition != Vector3.zero) {
+            transform.position = Vector3.MoveTowards(transform.position, jupmTargetPosition, jumpSpeed);
+            if(Vector3.Distance(transform.position, jupmTargetPosition) < 0.1f) {
+                jupmTargetPosition = Vector3.zero;
+                bubbleProjections.Do();
+            }
+        }
+    }
 
     public void Breath() {
         breathOfBubbles.Do();
     }
 
     public void JumpTo(Vector3 targetPosition) {
-        bubbleProjections.Do();
+        jupmTargetPosition = targetPosition;
+        Thread.Sleep(2000);
     }
 }
