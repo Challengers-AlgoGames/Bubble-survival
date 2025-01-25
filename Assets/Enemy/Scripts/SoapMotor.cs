62a803168f9fffea8c9936cc462cb34aafa7a28a
HerizoRand <hontyherizorandrianarisoa@yahoo.com>
2025-01-25 10:24:14 +0300

    Camera Shake System update

--- Assets/Enemy/Scripts/SoapMotor.cs
+++ Assets/Enemy/Scripts/SoapMotor.cs
@@ -4,43 +4,49 @@ public class SoapMotor : MonoBehaviour
 {
     [SerializeField] BubbleProjections bubbleProjections;
     [SerializeField] BreathOfBubbles breathOfBubbles;
+    [SerializeField] CameraManager cameraManager;
     [SerializeField] float jumpSpeed = 10f;
 
     private Vector3 impactPoint = Vector3.zero;
     private Vector3 outOfSceen = new Vector3(0, 12, 0);
     private bool isOutIfSceen = false;
 
-    void FixedUpdate() {
-        
-        if(impactPoint != Vector3.zero) 
-        {
-            float speed = jumpSpeed*2f;
+    void FixedUpdate()
+    {
 
-            if(isOutIfSceen) 
+        if (impactPoint != Vector3.zero)
+        {
+            float speed = jumpSpeed * 2f;
+
+            if (isOutIfSceen)
             {
                 transform.position = Vector3.MoveTowards(transform.position, impactPoint, speed * Time.fixedDeltaTime);
-                if(Vector3.Distance(transform.position, impactPoint) < 0.1f) 
+                if (Vector3.Distance(transform.position, impactPoint) < 0.1f)
                 {
                     bubbleProjections.Do();
                     impactPoint = Vector3.zero;
                     isOutIfSceen = false;
+                    StartCoroutine(cameraManager.Shake(0.5f, 0.3f));
                 }
-            } 
-            else 
+            }
+            else
             {
                 transform.position = Vector3.MoveTowards(transform.position, outOfSceen, speed * Time.fixedDeltaTime);
-                if(Vector3.Distance(transform.position, outOfSceen) < 0.1f) 
+                if (Vector3.Distance(transform.position, outOfSceen) < 0.1f)
                     isOutIfSceen = true;
             }
         }
     }
 
-    public void Breath() {
+    public void Breath()
+    {
         breathOfBubbles.Do();
     }
 
-    public void JumpTo(Vector3 targetPosition) {
-        if(impactPoint == Vector3.zero) {
+    public void JumpTo(Vector3 targetPosition)
+    {
+        if (impactPoint == Vector3.zero)
+        {
             impactPoint = targetPosition;
         }
     }
